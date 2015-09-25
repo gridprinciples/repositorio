f06846acabc043c970c387776341a69579e6a460
gbrock <greg@gridprinciples.com>
2015-09-25 00:08:22 -0400

    Adding comments, renaming file for clarity

--- src/RepositoryInterface.php
+++ src/RepositoryInterface.php
@@ -4,11 +4,35 @@ namespace GridPrinciples\Repository;
 
 interface RepositoryInterface
 {
+    /**
+     * Retrieve a target (or targets) by ID.
+     *
+     * @param $targets
+     */
     public static function get($targets);
 
+    /**
+     * Get a list of records.
+     *
+     * @param int $limit
+     * @return
+     */
     public static function index($limit = 15);
 
+    /**
+     * Creates or updates one or many models.
+     *
+     * @param mixed $data
+     * @param mixed $targets
+     * @return Collection
+     */
     public static function save($data, $targets = false);
 
+    /**
+     * Delete one or many models.
+     *
+     * @param mixed $targets
+     * @return boolean
+     */
     public static function delete($targets);
 }
